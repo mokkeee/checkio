daafff39f95182463f51e33d0d484f93c0bfcc10
Satoru Motegi <mokky.monkey.14@gmail.com>
2014-03-08 00:14:24 +0900

    3/7 solved. refactor.

--- the_end_of_other.py
+++ the_end_of_other.py
@@ -5,9 +5,8 @@ def checkio(words_set):
     sorted_words_set = sorted(words_set, key=len, reverse=False)
     while len(sorted_words_set) > 1:
         word = sorted_words_set.pop(0)
-        for compare_set in sorted_words_set:
-            if compare_set.endswith(word):
-                return True
+        if [x for x in sorted_words_set if x.endswith(word)]:
+            return True
     else:
         return False
 
