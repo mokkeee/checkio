e4fb768cf7c2f91cb0ec605c52026847f4f1936e
Satoru Motegi <mokky.monkey.14@gmail.com>
2014-03-14 00:35:57 +0900

    3/14 solved.

--- open_labyrinth.py
+++ open_labyrinth.py
@@ -1,45 +1,49 @@
 __author__ = 'mokkeee'
 
-# Go clockwise
-ROUTINE = {"S": ("W", "S", "E", "N"),
-           "W": ("N", "W", "S", "E"),
-           "N": ("E", "N", "W", "S"),
-           "E": ("S", "E", "N", "W")}
 MOVE = {"S": (1, 0), "N": (-1, 0), "W": (0, -1), "E": (0, 1)}
-
+DIRECTIONS = ["E", "S", "W", "N"]
+OPPOSITE_DIRECTION = {"N":"S", "S":"N", "W":"E", "E":"W"}
+GOAL = (10, 10)
 
 def next_pos(current, add):
     return (current[0] + add[0], current[1] + add[1])
 
+def routing(maze_map, pos, direction, route, passed):
+    if direction is not None:
+        directions_candidate = DIRECTIONS[:]
+        directions_candidate.remove(OPPOSITE_DIRECTION[direction])
+    else:
+        directions_candidate = DIRECTIONS
+
+    for d in directions_candidate:
+        tmp_pos = next_pos(pos, MOVE[d])
+        # enable route
+        if maze_map[tmp_pos[0]][tmp_pos[1]] == 0:
+            # GOAL!
+            if tmp_pos == GOAL:
+                print(route + d)
+                return route + d
+
+            # rope detect. route discard.
+            if tmp_pos in passed:
+                return None
+
+            # add passed route
+            tmp_passed = passed[:]
+            tmp_passed.append(tmp_pos)
+
+            # search route recursively
+            r = routing(maze_map, tmp_pos, d, route + d, tmp_passed)
+            if r is not None:
+                return r
+    # dead end.
+    else:
+        return None
+
 
 def checkio(maze_map):
-    direction = "N"
-    route = ""
     pos = (1, 1)
-    goal = (10, 10)
-    passed = [pos]
-    while pos != goal:
-        for d in ROUTINE[direction]:
-            tmp_pos = next_pos(pos, MOVE[d])
-            if maze_map[tmp_pos[0]][tmp_pos[1]] == 0:
-                if tmp_pos in passed:
-                    direction = ROUTINE[direction][-1]
-                    route += d
-                    passed.append(pos)
-                    print("R:" + route)
-                    print("passwd:" + str(passed))
-                    break
-                pos = tmp_pos
-                route += d
-                direction = d
-                passed.append(pos)
-                print("R:" + route)
-                print("passwd:" + str(passed))
-                break
-        else:
-            assert False, "loop detect."
-
-    return route
+    return routing(maze_map, pos, None, "", [])
 
 
 if __name__ == '__main__':
