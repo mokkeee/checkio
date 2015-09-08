bec6487faf8bb18e0e340a438985f1aca92deac5
Satoru Motegi <mokky.monkey.14@gmail.com>
2015-09-08 18:05:04 +0900

    refactoring code.

--- most_alphabet.py
+++ most_alphabet.py
@@ -1,18 +1,15 @@
-__author__ = 'motegi'
 import string
 
 def checkio(text):
     text = text.lower().strip()
-    char_and_count = []
+    char_and_count = {}
     for c in string.ascii_lowercase:
-        char_count = text.count(c)
-        if char_count != 0:
-            char_and_count.append((c, char_count))
-    char_and_count.sort(key=lambda count: count[1], reverse=True)
-
-    print(char_and_count)
-
-    return char_and_count[0][0]
+        char_and_count[c] = text.count(c)
+    
+    max_count = max(char_and_count.values())
+    max_count_chars = {k:v for (k,v) in char_and_count.items() if v == max_count }
+    
+    return min(max_count_chars.keys())
 
 #These "asserts" using only for self-checking and not necessary for auto-testing
 if __name__ == '__main__':
