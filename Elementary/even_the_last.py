9e83be81d86de650dc4c445411ddb769c2c5d169
mokkeee <mtg-gh-99078>
2015-09-12 04:28:27 +0900

    refactor

--- Elementary/even_the_last.py
+++ Elementary/even_the_last.py
@@ -4,7 +4,7 @@ def checkio(array):
     if len(array) == 0:
         return 0
 
-    return sum(array[0:len(array):2]) * array[-1]
+    return sum(array[0::2]) * array[-1]
 
 
 #These "asserts" using only for self-checking and not necessary for auto-testing
