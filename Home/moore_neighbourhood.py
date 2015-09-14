bd85606fbd1163e7265da147cae9b3f92dd7f9b5
mokkeee <mtg-gh-99078>
2015-09-15 07:57:59 +0900

    2015/9/15 cleared. (need refactoring)

---
+++ Home/moore_neighbourhood.py
@@ -0,0 +1,50 @@
+__author__ = 'mokkeee'
+
+
+def is_enable(grid, row, col):
+    if row < 0 or col < 0:
+        return False
+    if row >= len(grid):
+        return False
+    if col >= len(grid[row]):
+        return False
+    return True
+
+
+def get_neighbour(grid, row, col):
+    if not is_enable(grid, row, col):
+        return None
+    return grid[row][col]
+
+
+def count_neighbours(grid, row, col):
+    neighbours = []
+    for i in range(col - 1, col + 2):
+        neighbours.append(get_neighbour(grid, row - 1, i))
+        neighbours.append(get_neighbour(grid, row + 1, i))
+
+    neighbours.append(get_neighbour(grid, row, col - 1))
+    neighbours.append(get_neighbour(grid, row, col + 1))
+
+    # print(neighbours)
+    return neighbours.count(1)
+
+
+if __name__ == '__main__':
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+    assert count_neighbours(((1, 0, 0, 1, 0),
+                             (0, 1, 0, 0, 0),
+                             (0, 0, 1, 0, 1),
+                             (1, 0, 0, 0, 0),
+                             (0, 0, 1, 0, 0),), 1, 2) == 3, "1st example"
+    assert count_neighbours(((1, 0, 0, 1, 0),
+                             (0, 1, 0, 0, 0),
+                             (0, 0, 1, 0, 1),
+                             (1, 0, 0, 0, 0),
+                             (0, 0, 1, 0, 0),), 0, 0) == 1, "2nd example"
+    assert count_neighbours(((1, 1, 1),
+                             (1, 1, 1),
+                             (1, 1, 1),), 0, 2) == 3, "Dense corner"
+    assert count_neighbours(((0, 0, 0),
+                             (0, 1, 0),
+                             (0, 0, 0),), 1, 1) == 0, "Single"
